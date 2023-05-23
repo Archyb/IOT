004661d3bb02068749edc2b810f1ca06df9e9de6
Archyb <121549285+Archyb@users.noreply.github.com>
2023-05-23 14:36:01 +0200

    add post allowed creation

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Grossiste, Revendeur, Marchandise, ConditionTransport, Document, Transport
 
+
 class GrossisteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grossiste
@@ -41,3 +42,32 @@ class TransportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transport
         fields = '__all__'
+
+    def create(self, validated_data):
+        grossiste_data = validated_data.pop('grossiste')
+        revendeur_data = validated_data.pop('revendeur')
+        marchandise_data = validated_data.pop('marchandise')
+        conditions_transport_data = validated_data.pop('conditions_transport')
+        documents_data = validated_data.pop('documents')
+
+        grossiste = Grossiste.objects.create(**grossiste_data)
+        revendeur = Revendeur.objects.create(**revendeur_data)
+        marchandise = Marchandise.objects.create(**marchandise_data)
+        conditions_transport = ConditionTransport.objects.create(**conditions_transport_data)
+
+        documents = []
+        for document_data in documents_data:
+            document = Document.objects.create(**document_data)
+            documents.append(document)
+
+        transport = Transport.objects.create(
+            grossiste=grossiste,
+            revendeur=revendeur,
+            marchandise=marchandise,
+            conditions_transport=conditions_transport,
+            **validated_data
+        )
+
+        transport.documents.set(documents)
+
+        return transport
--- api/views.py
+++ api/views.py
@@ -11,11 +11,34 @@ def transport_view(request):
         serializer = TransportSerializer(transports, many=True)
         return Response(serializer.data)
 
+
+from rest_framework.decorators import api_view
+
+from rest_framework.response import Response
+
+from .serializers import TransportSerializer
+
+
+@api_view(['GET', 'POST'])
+def transport_view(request):
+    if request.method == 'GET':
+
+        transports = Transport.objects.all()
+
+        serializer = TransportSerializer(transports, many=True)
+
+        return Response(serializer.data)
+
+
     elif request.method == 'POST':
+
         serializer = TransportSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
+
             return Response(serializer.data, status=201)
+
         return Response(serializer.errors, status=400)
 
 
