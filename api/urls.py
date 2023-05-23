985616341fe358024c3142b7bdd7f1eb3a8eb5c8
Archyb <121549285+Archyb@users.noreply.github.com>
2023-05-23 17:29:39 +0200

    modify endpoint url

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('transport/', views.transport_view, name='transport'),
+    path('epcis/', views.transport_view, name='transport'),
 ]
